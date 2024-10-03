361d57bcbfa8ba1e2d41d08125281eb58613ea71
leticia080 <126690016+leticia080@users.noreply.github.com>
2024-10-03 10:26:15 -0300

    03-10-24

--- script.js
+++ script.js
@@ -54,9 +54,9 @@ mostraAlternativas();
 function mostraAlternativas() {
 for (const alternativa of perguntaAtual.alternativas) {
 const botaoAlternativas = document.createElement("button");
-botaoAlternativas.textContent = alternativa;
+botaoAlternativas.textContent = alternativa.texto;
+botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa)){
 caixaAlternativas.appendChild(botaoAlternativas);
-}
 }
 
 mostraPergunta();
